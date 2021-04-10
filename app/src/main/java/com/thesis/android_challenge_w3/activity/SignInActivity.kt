4ece6d4d2a516931e0c3993cdcd8870e7f54e0d4
ndv260599 <nguyenducvu26051999@gmail.com>
2021-04-10 13:48:33 +0700

    viewmodel + livedata Sign in

--- app/src/main/java/com/thesis/android_challenge_w3/activity/SignInActivity.kt
+++ app/src/main/java/com/thesis/android_challenge_w3/activity/SignInActivity.kt
@@ -6,6 +6,7 @@ import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.thesis.android_challenge_w3.R
 import com.thesis.android_challenge_w3.databinding.ActivitySignInBinding
@@ -30,25 +31,9 @@ class SignInActivity : AppCompatActivity() {
         )
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
         viewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
-
+        binding.signInViewModel = viewModel
+        setupViewModel()
         binding.apply {
-
-            btnSignIn.setOnClickListener {
-                viewModel.user.email = edtEmail.text.toString().trim()
-                viewModel.user.password = edtPassword.text.toString().trim()
-                if(DataStore.instance.isLoginSucceed(viewModel.user.email, viewModel.user.password)){
-                    Toast.makeText(this@SignInActivity, "Sign in Succeed", Toast.LENGTH_SHORT).show()
-
-                    val bundle = Bundle()
-                    bundle.putParcelable(USER_KEY, DataStore.instance.currentUser)
-                    val intent = Intent(this@SignInActivity, ProfileActivity::class.java)
-                    intent.putExtras(bundle)
-                    startActivity(intent)
-                } else {
-                    Toast.makeText(this@SignInActivity, "Sign in failed", Toast.LENGTH_SHORT).show()
-
-                }
-            }
 
             btnSignUp.setOnClickListener {
                 val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
@@ -58,4 +43,26 @@ class SignInActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupViewModel() {
+        viewModel.isSignInSuccess.observe(this, Observer<Boolean> {
+            if (it) {
+                showToast("Sign in Successful")
+                startProfileActivity()
+            } else {
+                showToast("Sign in failed")
+            }
+        })
+    }
+    private fun startProfileActivity() {
+        val bundle = Bundle()
+        bundle.putParcelable(USER_KEY, DataStore.instance.currentUser)
+        val intent = Intent(this@SignInActivity, ProfileActivity::class.java)
+        intent.putExtras(bundle)
+        startActivity(intent)
+    }
+
+    private fun showToast(value: String) {
+        Toast.makeText(this@SignInActivity, "" + value, Toast.LENGTH_SHORT).show()
+
+    }
 }
