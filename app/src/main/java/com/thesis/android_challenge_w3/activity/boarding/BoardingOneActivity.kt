9ea0f3647b9997b16b108d4ad76c2d3de08ba057
Viet Hua <huaviet999@gmail.com>
2021-04-12 14:10:52 +0700

    add scroll view + gif file

--- app/src/main/java/com/thesis/android_challenge_w3/activity/boarding/BoardingOneActivity.kt
+++ app/src/main/java/com/thesis/android_challenge_w3/activity/boarding/BoardingOneActivity.kt
@@ -14,6 +14,7 @@ import  com.thesis.android_challenge_w3.R
 class BoardingOneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_onboarding_one)
 
         val btnNext = findViewById<ImageButton>(R.id.btn_next)
